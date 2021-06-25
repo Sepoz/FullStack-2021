e62c475f40791b6207ae53b6876539a10b9f7752
Simone Boccolari <simone.boccolari@hotmail.it>
2021-06-25 14:33:26 +0200

    exercise 4.4

--- part4/blogPost/utils/list_helper.js
+++ part4/blogPost/utils/list_helper.js
@@ -1,7 +1,17 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
     return 1;
 };
 
+const totalLikes = (blogs) => {
+    let likes = 0;
+    blogs.map((blog) => {
+        likes = likes + blog.likes;
+    });
+    return likes;
+};
+
 module.exports = {
     dummy,
+    totalLikes,
 };
