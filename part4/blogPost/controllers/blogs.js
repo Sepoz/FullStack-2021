2c26ddab17b3dd622f9b9c1a0f16c27003ee82e6
Simone Boccolari <simone.boccolari@hotmail.it>
2021-06-29 00:41:18 +0200

    exercise 4.19

--- part4/blogPost/controllers/blogs.js
+++ part4/blogPost/controllers/blogs.js
@@ -1,6 +1,8 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const config = require("../utils/config");
 
 blogsRouter.get("/", async (request, response) => {
     const blogs = await Blog.find({}).populate("user", {
@@ -10,16 +12,30 @@ blogsRouter.get("/", async (request, response) => {
     response.json(blogs);
 });
 
+const getTokenFrom = (request) => {
+    const authorization = request.get("authorization");
+
+    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+        return authorization.substring(7);
+    }
+    return null;
+};
+
 blogsRouter.post("/", async (request, response) => {
     const body = request.body;
+
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, config.SECRET);
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: "token missing or invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
 
     if (body.likes === undefined) {
         body.likes = 0;
     } else if (body.title === undefined || body.url === undefined) {
         return response.status(400).json({ error: "missing title or url" });
     }
-
-    const user = await User.findById(body.userId);
 
     const blog = new Blog({
         title: body.title,
