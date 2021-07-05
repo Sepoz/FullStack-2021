fa6fc01de41fa54df708aeb88903dfe71a6c08db
Simone Boccolari <simone.boccolari@hotmail.it>
2021-07-05 16:05:41 +0200

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Blog = (props) => {
-    const { title, author } = props.blog;
+    const { title, author, url, likes } = props.blog;
 
-    return (
-        <div>
-            {title} {author}
-        </div>
-    );
+    const [showInfo, setShowInfo] = useState(false);
+
+    const styleBlog = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: "solid",
+        borderWidth: 1,
+        marginBottom: 5,
+    };
+
+    const handleShowInfo = () => {
+        setShowInfo(!showInfo);
+    };
+
+    if (showInfo === false) {
+        return (
+            <div style={styleBlog}>
+                <p>
+                    {title} {author}
+                </p>
+                <button onClick={handleShowInfo}>show</button>
+            </div>
+        );
+    } else {
+        return (
+            <div style={styleBlog}>
+                <p>
+                    {title} {author}
+                </p>
+                <p>{url}</p>
+                <p>{likes}</p>
+                <button>like</button>
+                <button onClick={handleShowInfo}>hide</button>
+            </div>
+        );
+    }
 };
 
 export default Blog;
